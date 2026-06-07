3b39733f72f3bace32f1f44842468915b30e8c86
yifan923 <wtl08@Proton.me>
2026-06-07 11:00:18 +0800

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,1 +1,19 @@
 
+#!/bin/bash
+
+# 出现错误立即退出
+set -e
+
+# 创建目录
+mkdir -p data
+
+echo "更新中国电信..."
+bgpq4 -4 AS4134 > data/ct.txt
+
+echo "更新中国移动..."
+bgpq4 -4 AS9808 > data/cm.txt
+
+echo "更新中国联通..."
+bgpq4 -4 AS4837 > data/cu.txt
+
+echo "完成"
